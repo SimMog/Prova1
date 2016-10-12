70950a04c9fced7cf6dffa6ab99088f0483675d1
SimMog <simimog@gmail.com>
2016-10-12 19:10:15 +0200

    Primo progetto caricato

---
+++ app/src/main/java/com/example/vova/prova1/testClass.java
@@ -0,0 +1,12 @@
+package com.example.vova.prova1;
+
+/**
+ * Created by vova on 12/10/16.
+ */
+
+public class testClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
